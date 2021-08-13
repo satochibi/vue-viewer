befbebbed9f071604e660848ea4260e2b3a1292b
satochibi <satochibi0120@gmail.com>
2021-08-13 12:09:12 +0900

    サムネイルとビューアーのvueファイルを作成

---
+++ src/components/thumbnail.vue
@@ -0,0 +1,82 @@
+<template>
+  <ul>
+    <li v-for="(image, index) in images" v-bind:key="index">
+      <div>
+        <img
+          v-bind:src="image.thumbnail"
+          v-bind:alt="'サムネイル' + (index + 1) + 'ページ目'"
+          v-on:click="onclick(index)"
+        />
+        <span>{{ index }}ページ</span>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "thumbnail",
+  data() {
+    return {
+      index: 0,
+      images: [
+        {
+          name: "1ページ目",
+          src: "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/1_001.jpg",
+          download: "ダウンロード1",
+          print: "プリント1",
+          thumbnail:
+            "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/thumb1_001.jpg",
+        },
+        {
+          name: "2ページ目",
+          src: "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/1_002.jpg",
+          download: "ダウンロード2",
+          print: "プリント2",
+          thumbnail:
+            "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/thumb1_002.jpg",
+        },
+        {
+          name: "3ページ目",
+          src: "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/1_003.jpg",
+          download: "ダウンロード3",
+          print: "プリント3",
+          thumbnail:
+            "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/thumb1_003.jpg",
+        },
+      ],
+    };
+  },
+  methods:{
+    onclick: function(clickedIndex){
+      this.index = clickedIndex;
+      console.log(this.index);
+    }
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  font-size: 0;
+  list-style: none;
+  padding: 0;
+  margin: 10px auto;
+}
+ul li {
+  font-size: 14px;
+  display: block;
+  padding-bottom: 10px;
+}
+
+img{
+  display: block;
+  margin: 0 auto;
+}
+
+span {
+  display: block;
+  text-align: center;
+}
+
+</style>
---
+++ src/components/viewer.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="overlay">
+    <div id="contents">
+      <div id="photoarea">
+        <div class="navigation" id="nav-close">
+          <button>X</button>
+        </div>
+        <div class="navigation" id="nav-left">
+          <button>←</button>
+        </div>
+        <div class="navigation" id="nav-right">
+          <button>→</button>
+        </div>
+        <img v-bind:src="images[index].src" v-bind:alt="images[index].name" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "viewer",
+  data() {
+    return {
+      index: 0,
+      images: [
+        {
+          name: "1ページ目",
+          src: "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/1_001.jpg",
+          download: "ダウンロード1",
+          print: "プリント1",
+          thumbnail:
+            "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/thumb1_001.jpg",
+        },
+        {
+          name: "2ページ目",
+          src: "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/1_002.jpg",
+          download: "ダウンロード2",
+          print: "プリント2",
+          thumbnail:
+            "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/thumb1_002.jpg",
+        },
+        {
+          name: "3ページ目",
+          src: "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/1_003.jpg",
+          download: "ダウンロード3",
+          print: "プリント3",
+          thumbnail:
+            "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/thumb1_003.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+div#overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+}
+
+div#contents {
+  position: absolute;
+  left: 10%;
+  top: 10%;
+  width: 80%;
+  height: 80%;
+  background: transparent;
+  /* 後で消す */
+  background: white;
+}
+
+div#photoarea {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+div#photoarea img {
+  display: block;
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.navigation {
+  position: absolute;
+}
+
+#nav-close {
+  right: 0;
+  top: 0;
+}
+
+#nav-left {
+  left: 0;
+  top: 50%;
+}
+#nav-right {
+  right: 0;
+  top: 50%;
+}
+</style>
